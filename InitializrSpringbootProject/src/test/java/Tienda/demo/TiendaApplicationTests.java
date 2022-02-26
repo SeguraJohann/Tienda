a4c70546d98f94e8d85c74c4a02459d53fe3a286
SeguraJohann <johann.segura.chacon@gmail.com>
2022-02-25 22:39:32 -0600

    Commit 2
    Hola mundo 2

--- InitializrSpringbootProject/src/test/java/Tienda/demo/TiendaApplicationTests.java
+++ InitializrSpringbootProject/src/test/java/Tienda/demo/TiendaApplicationTests.java
@@ -2,7 +2,7 @@ package Tienda.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//comentario
 @SpringBootTest
 class TiendaApplicationTests {
 
