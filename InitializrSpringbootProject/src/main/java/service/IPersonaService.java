6d7d961b4d1c578ac6388c03e31e60e072b816c8
SeguraJohann <johann.segura.chacon@gmail.com>
2022-03-19 00:13:46 -0600

    Update IPersonaService.java

--- InitializrSpringbootProject/src/main/java/service/IPersonaService.java
+++ InitializrSpringbootProject/src/main/java/service/IPersonaService.java
@@ -14,11 +14,14 @@ import org.springframework.stereotype.Repository;
  * @author Personal
  */
 
+
 @Repository
 public interface IPersonaService {
+    
     public List<Persona> getAllPerson();
     public void savePerson(Persona persona);
     public Persona getPersonById(long id);
     public void delete(long id);
     
 }
+
