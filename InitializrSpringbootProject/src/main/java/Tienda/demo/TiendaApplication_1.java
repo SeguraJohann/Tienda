ab62e77b86f2b64fa63e6511955a1e9381ee67ed
SeguraJohann <johann.segura.chacon@gmail.com>
2022-04-16 13:54:38 -0600

    16/04/2022

---
+++ InitializrSpringbootProject/src/main/java/Tienda/demo/TiendaApplication_1.java
@@ -0,0 +1,13 @@
+package Tienda.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaApplication_1 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaApplication_1.class, args);
+	}
+
+}
